debc98699e4db7b7cf6e4dbb8cb3b8c481b1eb20
Joyce <geojoyce934@gmail.com>
2017-01-01 20:40:37 +0530

    first commit of 2k17, 1 semicolon program

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Puzzles
+Happy New Year 2017
+Cpp program to print a statement by using only one semicolon in the main function.
---
+++ semiLess.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  if(cout<<"Happy New Year 2017"<<endl)
+    return 0;
+  
+}
